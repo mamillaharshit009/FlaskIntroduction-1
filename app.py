786ef2f5d895b0b6c2b2d9553455ba8107d5a42f
mamillaharshit009 <mamillaharshit009@gmail.com>
2024-11-18 18:18:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Sample in-memory task list
 tasks = []
